e0f9a3e61a33f0da70c514adc06f3e5ac094633b
Marius Lundgård <studio@mariuslundgard.com>
2022-12-01 19:07:23 +0100

    feat: debug context and grid

---
+++ components/debug/debug-provider.tsx
@@ -0,0 +1,37 @@
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react'
+
+const DebugContext = createContext({ grid: true })
+
+export function useDebug() {
+  return useContext(DebugContext)
+}
+
+export function DebugProvider(props: { children?: ReactNode }) {
+  const { children } = props
+  const [grid, setGrid] = useState(true)
+
+  const debug = useMemo(() => ({ grid }), [grid])
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === 'g') {
+        setGrid((grid) => !grid)
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyDown)
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [])
+
+  return <DebugContext.Provider value={debug}>{children}</DebugContext.Provider>
+}
