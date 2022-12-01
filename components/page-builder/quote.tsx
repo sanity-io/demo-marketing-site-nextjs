cb142c329f3168d73e60add1e3da72ff7b764cba
Marius Lundgård <studio@mariuslundgard.com>
2022-12-01 15:10:29 +0100

    fix: dark mode

--- components/page-builder/quote.tsx
+++ components/page-builder/quote.tsx
@@ -32,7 +32,7 @@ export default function PageBuilderQuote(props: QuoteProps) {
   return (
     <Container className="flex flex-col-reverse lg:flex-row lg:items-center">
       <div className="-mt-5 flex flex-col gap-5 md:mt-0 md:px-5 lg:w-1/2 lg:flex-row">
-        <div className="flex h-6 w-6 flex-shrink-0 items-center justify-center rounded-full border border-gray-200">
+        <div className="flex h-6 w-6 flex-shrink-0 items-center justify-center rounded-full border border-gray-200 dark:border-gray-900">
           <Quote className="h-5 w-5 flex-shrink-0" />
         </div>
         <div className="flex flex-col gap-5">
