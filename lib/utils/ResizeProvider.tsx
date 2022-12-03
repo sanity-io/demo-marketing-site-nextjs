1510e52ca82cfe2503b89c216c4839ebcc1f10d9
Snorre Eskeland Brekke <snorre.e.brekke@gmail.com>
2022-12-03 20:36:24 +0100

    fix: nullsafe resize provider when hotreloading

--- lib/utils/ResizeProvider.tsx
+++ lib/utils/ResizeProvider.tsx
@@ -13,8 +13,8 @@ export function ResizeProvider(props: {
 }) {
   const { children, rootElement = null } = props
 
-  const mo = useRef<MutationObserver>()
-  const ro = useRef<ResizeObserver>()
+  const mo = useRef<MutationObserver | undefined>()
+  const ro = useRef<ResizeObserver | undefined>()
   const elements = useRef<Element[]>([])
   const subscriberMap = useRef<WeakMap<Element, ResizeSubscriber>>(
     new WeakMap()
@@ -25,8 +25,6 @@ export function ResizeProvider(props: {
     if (!rootElement) return
 
     mo.current = new MutationObserver(() => {
-      console.log('mutate')
-
       for (const element of elements.current) {
         const subscriber = subscriberMap.current.get(element)
         if (subscriber) subscriber()
@@ -42,7 +40,7 @@ export function ResizeProvider(props: {
     })
 
     return () => {
-      ro.current.disconnect()
+      ro.current?.disconnect()
       ro.current = undefined
     }
   }, [rootElement])
@@ -53,14 +51,14 @@ export function ResizeProvider(props: {
       for (const entry of entries) {
         const element = entry.target
 
-        const next = subscriberMap.current.get(element)
+        const next = subscriberMap.current?.get(element)
 
         next(entry)
       }
     })
 
     return () => {
-      ro.current.disconnect()
+      ro.current?.disconnect()
       ro.current = undefined
     }
   }, [])
@@ -68,19 +66,19 @@ export function ResizeProvider(props: {
   const observe = useCallback((element: HTMLElement): ResizeObservable => {
     return {
       subscribe(next) {
-        elements.current.push(element)
-        ro.current.observe(element)
+        elements.current?.push(element)
+        ro.current?.observe(element)
 
-        subscriberMap.current.set(element, next)
+        subscriberMap.current?.set(element, next)
 
         return {
           unsubscribe() {
-            ro.current.unobserve(element)
+            ro.current?.unobserve(element)
 
-            const idx = elements.current.indexOf(element)
+            const idx = elements.current?.indexOf(element)
 
             if (idx > -1) {
-              elements.current.splice(idx, 1)
+              elements.current?.splice(idx, 1)
             }
           },
         }
