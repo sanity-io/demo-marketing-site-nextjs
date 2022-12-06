40fb650322984cc7814dde04b63af5d60b1a99ff
Snorre Eskeland Brekke <snorre.e.brekke@gmail.com>
2022-12-06 11:35:10 +0100

    fix: tweaks

--- components/page-builder/bento/bento-1/BentoSubtitle.tsx
+++ components/page-builder/bento/bento-1/BentoSubtitle.tsx
@@ -3,67 +3,35 @@ import React from 'react'
 
 export function BentoSubtitle({
   subtitle,
-  type = 'plain',
   className,
 }: {
   subtitle?: string
-  type?: 'pill' | 'plain'
   className?: string
 }) {
   if (!subtitle) {
     return null
   }
 
-  const Component = type === 'plain' ? Plain : Pill
   return (
     <div className={className ?? 'py-2'}>
-      <Component subtitle={subtitle} />
+      <m.div
+        initial={{
+          opacity: 0,
+        }}
+        whileInView={{
+          opacity: 1,
+        }}
+        viewport={{
+          amount: 'all',
+          margin: '100px',
+        }}
+        transition={{ duration: 1 }}
+        className={
+          'max-w-xl rounded-full text-2xl text-magenta-500 dark:text-magenta-400'
+        }
+      >
+        {subtitle}
+      </m.div>
     </div>
-  )
-}
-
-function Pill({ subtitle }: { subtitle: string }) {
-  return (
-    <m.div
-      initial={{
-        scale: 0,
-      }}
-      whileInView={{
-        scale: 1,
-      }}
-      viewport={{
-        amount: 'all',
-        margin: '100px',
-      }}
-      transition={{ type: 'spring' }}
-      className={
-        'max-w-xl rounded-full bg-magenta-400 px-5 py-2 text-2xl text-black'
-      }
-    >
-      {subtitle}
-    </m.div>
-  )
-}
-
-function Plain({ subtitle }: { subtitle: string }) {
-  return (
-    <m.div
-      initial={{
-        opacity: 0,
-      }}
-      whileInView={{
-        opacity: 1,
-      }}
-      viewport={{
-        amount: 'all',
-        margin: '100px',
-      }}
-      transition={{ duration: 2 }}
-      className={
-        'max-w-xl rounded-full text-2xl text-magenta-500 dark:text-magenta-400'
-      }
-    >
-      {subtitle}
-    </m.div>
   )
 }
